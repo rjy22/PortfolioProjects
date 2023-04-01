0980a1ff67534f785238d3105f13f9443a065ba6
Rick Yang <37157937+rjy22@users.noreply.github.com>
2023-03-31 21:37:48 -0700

    Update Udemy MySQL Task 4.sql
    First edit

--- Udemy SQL-Tableau Integration Project/Udemy MySQL Task 4.sql
+++ Udemy SQL-Tableau Integration Project/Udemy MySQL Task 4.sql
@@ -11,18 +11,18 @@ DROP PROCEDURE IF EXISTS avg_dept_salary;
 DELIMITER $$
 CREATE PROCEDURE avg_dept_salary(IN p_lower_salary_limit FLOAT, IN p_upper_salary_limit FLOAT)
 BEGIN
-	SELECT
-		m.gender,
+    SELECT
+        m.gender,
         ROUND(AVG(s.salary),2) AS avg_salary,
         d.dept_name
     FROM t_employees AS m
     JOIN t_salaries AS s
-		ON m.emp_no = s.emp_no
-	JOIN t_dept_emp AS de
-		ON s.emp_no = de.emp_no
-	JOIN t_departments AS d
-		ON de.dept_no = d.dept_no
-	WHERE s.salary BETWEEN p_lower_salary_limit AND p_upper_salary_limit
+	ON m.emp_no = s.emp_no
+    JOIN t_dept_emp AS de
+	ON s.emp_no = de.emp_no
+    JOIN t_departments AS d
+	ON de.dept_no = d.dept_no
+    WHERE s.salary BETWEEN p_lower_salary_limit AND p_upper_salary_limit
     GROUP BY m.gender, d.dept_no
     ORDER BY d.dept_name, avg_salary DESC;
 END $$
