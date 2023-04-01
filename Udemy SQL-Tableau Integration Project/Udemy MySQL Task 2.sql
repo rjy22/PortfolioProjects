bcb881faa543b7648c1a93d4d7908bdfdcc8366c
Rick Yang <37157937+rjy22@users.noreply.github.com>
2023-03-31 21:34:29 -0700

    Update Udemy MySQL Task 2.sql
    First edit

--- Udemy SQL-Tableau Integration Project/Udemy MySQL Task 2.sql
+++ Udemy SQL-Tableau Integration Project/Udemy MySQL Task 2.sql
@@ -4,29 +4,32 @@ from different departments for each year,
 starting from 1990.
 */
 
-SELECT
-	d.dept_name,
+SELECT 
+    d.dept_name,
     m.gender,
     dm.emp_no,
     dm.from_date,
     dm.to_date,
     e.calendar_year,
     (CASE
-		WHEN e.calendar_year BETWEEN YEAR(dm.from_date) AND YEAR(dm.to_date) THEN 1
+	WHEN e.calendar_year BETWEEN YEAR(dm.from_date) AND YEAR(dm.to_date) THEN 1
         ELSE 0
      END) AS active_year
 FROM
-	(SELECT
-		YEAR(hire_date) AS calendar_year
-	 FROM 
-		t_employees
-	 GROUP BY calendar_year) AS e
-	CROSS JOIN
-		t_dept_manager AS dm
-	JOIN t_departments AS d
-		ON dm.dept_no = d.dept_no
-	JOIN t_employees AS m
-		ON dm.emp_no = m.emp_no
+    (SELECT
+         YEAR(hire_date) AS calendar_year
+     FROM 
+	 t_employees
+     GROUP BY 
+	 calendar_year) AS e
+CROSS JOIN
+     t_dept_manager AS dm
+JOIN 
+     t_departments AS d
+    ON dm.dept_no = d.dept_no
+JOIN 
+     t_employees AS m
+    ON dm.emp_no = m.emp_no
 ORDER BY
-	dm.emp_no,
+    dm.emp_no,
     e.calendar_year;
